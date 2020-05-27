b192ff90e9e88a04e551286a2180fefcecb97d96
bcat95 <bcat9505@gmail.com>
2020-05-27 11:04:55 +0700

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# name: discourse-tvms-amp
+# about: Add amp link to website
+# version: 0.1
+# authors: DonAnh
+# url: https://github.com/bcat95/discourse-tvms-amp/
+
+PLUGIN_NAME = "discourse-tvms-amp".freeze
+
+enabled_site_setting :tvms_amp_enabled
